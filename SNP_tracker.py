9adc41d3f2a90cedae2658dc3e39c39ecf54a9a9
SamHeysmond <sheysmond@outlook.com>
2024-06-03 22:07:50 +0100

    R script write and exe
    R files are now written and executed for average manhattan plots

--- SNP_tracker.py
+++ SNP_tracker.py
@@ -1,6 +1,9 @@
-# This file tracks:
-#           1) Hand selected SNPs depending on the phenotype
-#           2) Top 20 significant SNPs in both GWAS and GIFT for each test
+# File version: 03_06_2024
+
+# This file:
+#   1) Tracks Hand selected SNPs depending on the phenotype  (X)
+#   2) Tracks Top 20 significant SNPs in both GWAS and GIFT for each test (X)
+#   3) Stores all SNPs for GWAS and GIFT, averaging them and making a csv for R to use (yes)
 
 
 
@@ -112,7 +115,7 @@ positive_control_Mo98_df = pandas.DataFrame(columns=[
                                                     'N_GIFT_TESTS'
                                                     ])
 
-# dataframe for ALL SNPs for IDEA 3
+# dataframe for ALL SNPs for IDEA 3 (FOR Mo98)
 Mo98_ALL_SNPS_GIFT_df = pandas.DataFrame(columns=[
                                                     'CHR', 
                                                     'POS',
@@ -132,6 +135,28 @@ Mo98_ALL_SNPS_GWAS_df = pandas.DataFrame(columns=[
                                                     'TIMES_APPEARED', # COULD be 90 or 99 or 100 or 10 who knows
                                                     'TOTAL_GWAS'   #SHOULD be 100
                                                     ])
+
+Na23_ALL_SNPS_GIFT_df = pandas.DataFrame(columns=[
+                                                    'CHR', 
+                                                    'POS',
+                                                    'SUBSAMPLE_NUM',  # for each subsamp number 200-1000
+                                                    'TOTAL_PSNP4',
+                                                    'TOTAL_PSNP5',
+                                                    'TOTAL_ABS_THETA',
+                                                    'TIMES_APPEARED', # COULD be 90 or 99 or 100 or 10 who knows
+                                                    'TOTAL_GIFT'   #SHOULD be 100
+                                                    ])
+
+Na23_ALL_SNPS_GWAS_df = pandas.DataFrame(columns=[
+                                                    'CHR', 
+                                                    'POS',
+                                                    'SUBSAMPLE_NUM',  # for each subsamp number 200-1000
+                                                    'TOTAL_P',
+                                                    'TIMES_APPEARED', # COULD be 90 or 99 or 100 or 10 who knows
+                                                    'TOTAL_GWAS'   #SHOULD be 100
+                                                    ])
+
+
 
 # further dataframes can then look at these SNPs and compare their n_sig values between GIFT and GWAS at DIFFERENT subsample nums
 
@@ -221,7 +246,6 @@ def GATHER_ALL_GIFT_SNPS(current_phenotype_df, ALL_GIFT_SNPS_df,Total_GIFT,curre
     print("GIFT GATHER function done")
     return ALL_GIFT_SNPS_df, Total_GIFT
 
-
 def GATHER_ALL_GWAS_SNPS(current_phenotype_df,ALL_GWAS_SNPS_df,Total_GWAS,current_subsample_num):
     
     # increment the GWAS total counter
@@ -266,29 +290,122 @@ def GATHER_ALL_GWAS_SNPS(current_phenotype_df,ALL_GWAS_SNPS_df,Total_GWAS,curren
             # re-sort the df so CHR and POS go in ascending order -> it looks nicer that way.
             ALL_GWAS_SNPS_df.sort_values(by=["CHR","POS"], axis=0, ascending=[True,True],inplace=True, na_position='first')
     print("GWAS GATHER function finished")
+    return ALL_GWAS_SNPS_df, Total_GWAS
 
-Total_GIFT = 0
-Total_GWAS = 0
+def CALCULATE_AVERAGE_SNPS_GIFT(ALL_GIFT_SNPS_df):
+    ALL_GIFT_SNPS_df["AVERAGE_PSNP4"] = ALL_GIFT_SNPS_df["TOTAL_PSNP4"] / ALL_GIFT_SNPS_df["TIMES_APPEARED"]
+    ALL_GIFT_SNPS_df["AVERAGE_PSNP5"] = ALL_GIFT_SNPS_df["TOTAL_PSNP5"] / ALL_GIFT_SNPS_df["TIMES_APPEARED"]
+    ALL_GIFT_SNPS_df["AVERAGE_ABS_THETA"] =ALL_GIFT_SNPS_df["TOTAL_ABS_THETA"] / ALL_GIFT_SNPS_df["TIMES_APPEARED"]
 
+    return ALL_GIFT_SNPS_df
+
+def CALCULATE_AVERAGE_SNPS_GWAS(ALL_GWAS_SNPS_df):
+    ALL_GWAS_SNPS_df["AVERAGE_P"] = ALL_GWAS_SNPS_df["TOTAL_P"] / ALL_GWAS_SNPS_df["TIMES_APPEARED"]
+
+    return ALL_GWAS_SNPS_df
+
+def MAKE_R_SCRIPT(phenotype,subsample_number,pval_type):
+    # make R script for each P value type (pSNP4, pSNP5, abs theta) -> check phys gwas for things like abs theta incase different
+    R_out=open("output_files/"+str(phenotype)+"_"+str(subsample_number)+"_"+str(pval_type)+"_AVG_MANHATTAN.R","w")
+    R_out.write(f'#R script for making manhattan plots with ggplot\n')
+    R_out.write(f'library("tidyverse")\n')
+    R_out.write(f'library("ggrepel")\n')   
+    R_out.write(f'\n')
+
+    # for GWAS data
+    if pval_type=="TOTAL_P":
+        R_out.write(f'GWAS_ALL_SNPS_DATA<-read.csv("output_files/{phenotype}_ALL_SNPS_GWAS.csv",header=true)\n')  
+        R_out.write(f'GWAS_SUBSAMPLE_{subsample_number}_SNPS_DATA <- subset(GWAS_ALL_SNPS_DATA,SUBSAMPLE_NUM=={subsample_number})\n') 
+    
+    # for GIFT data
+    else:
+        R_out.write(f'GIFT_ALL_SNPS_DATA<-read.csv("output_files/{phenotype}_ALL_SNPS_GWAS.csv",header=true)\n')  
+        R_out.write(f'{pval_type}_SUBSAMPLE_{subsample_number}_SNPS_DATA <- subset(GWAS_ALL_SNPS_DATA,SUBSAMPLE_NUM=={subsample_number})\n') 
+    
+    # cumulative calculations
+    R_out.write(f'don <- {pval_type}_SUBSAMPLE_{subsample_number}_SNPS_DATA %>%\n')
+    R_out.write(f'     # Compute CHR size\n')
+    R_out.write(f'     group_by(CHR) %>% \n')
+    R_out.write(f'     summarise(chr_len=max(POS)) %>%\n')
+    R_out.write(f'     # Calculate cumulative position of each CHR\n')
+    R_out.write(f'     mutate(tot=cumsum(chr_len)-chr_len) %>%\n')
+    R_out.write(f'     select(-chr_len) %>%\n')
+    R_out.write(f'     # Add this info to the initial dataset\n')
+    R_out.write(f'     left_join({pval_type}_SUBSAMPLE_{subsample_number}_SNPS_DATA, ., by=c("CHR"="CHR")) %>%\n')
+    R_out.write(f'     # Add a cumulative position of each SNP\n')
+    R_out.write(f'     arrange(CHR, POS) %>%\n')
+    R_out.write(f'     mutate( BPcum=POS+tot) \n')
+    R_out.write(f'axisdf = don %>%\n')
+    R_out.write(f'     group_by(CHR) %>%\n')
+    R_out.write(f'     summarize(center=( max(BPcum) + min(BPcum) ) / 2 )\n')
+
+    # set y limit for the graph (not sure if this changes anything here though- just copied from other script)
+    if pval_type=="TOTAL_ABS_THETA": #test
+        R_out.write(f'ylim <- abs(floor(min({pval_type}_SUBSAMPLE_{subsample_number}_SNPS_DATA${pval_type}))) +1\n')
+    else:
+        R_out.write(f'ylim <- abs(floor(log10(min({pval_type}_SUBSAMPLE_{subsample_number}_SNPS_DATA${pval_type})))) +1\n')
+
+    R_out.write(f'#open png\n')
+    R_out.write(f'png("output_files/{phenotype}_{subsample_number}_{pval_type}"_AVG_MANHATTAN.png", bg = "white", width = 9.75, height = 3.25, units = "in", res = 1200, pointsize = 4)\n')
+
+    # make the plot
+    if pval_type=="TOTAL_ABS_THETA":
+        R_out.write(f'ggplot(don, aes(x=BPcum, y=({pval_type}), color=as_factor(CHR))) +\n')
+    else:
+        R_out.write(f'ggplot(don, aes(x=BPcum, y=-log10({pval_type}), color=as_factor(CHR))) +\n')
+    R_out.write(f'     # Show all points\n')
+    R_out.write(f'     geom_point(alpha=0.5) +\n')
+    R_out.write(f'     # custom X axis:\n')
+    R_out.write(f'     scale_x_continuous( label = axisdf$CHR, breaks= axisdf$center ) +\n')
+    # TEMP PAUSE FOR TESTING
+    #Rscript_output.write(f'     scale_y_continuous(expand = c(0, 0) ) + # remove space between plot area and x axis\n')
+    # label axis
+    R_out.write(f'     # my axis labels\n')
+    if pval_type=="TOTAL_ABS_THETA":
+        R_out.write(f'     labs(y= "({pval_type})", x = "chromosome position")+\n')
+    else:
+        R_out.write(f'     labs(y= "-log10({pval_type})", x = "chromosome position")+\n')
+    # add a theme
+    R_out.write(f'     # Custom the theme:\n')
+    R_out.write(f'     theme_minimal() +\n')
+    R_out.write(f'     guides(colour="none")\n')
+    R_out.write(f'     theme(\n')
+    R_out.write(f'       panel.border = element_blank(),\n')
+    R_out.write(f'       panel.grid.major.x = element_blank(),\n')
+    R_out.write(f'       panel.grid.minor.x = element_blank(),\n')
+    R_out.write(f'       axis.text.x = element_text(angle = 90, size = 8, vjust = 0.5)\n')
+    R_out.write(f'      )\n')
+    R_out.write(f'dev.off()\n')
+    R_out.write(f'#END OF SCRIPT\n')
+    R_out.close()
+
+    os.system("output_files/"+str(phenotype)+"_"+str(subsample_number)+"_"+str(pval_type)+"_AVG_MANHATTAN.R")
+    return
+
+Total_GIFT_Mo98 = 0
+Total_GWAS_Mo98 = 0
+
+Total_GIFT_Na23 = 0
+Total_GWAS_Na23 = 0
 csv_files = os.listdir(args.d)
 
 for csv_file in csv_files:
 
+    # split on the underscore to make lists
     csv_file=csv_file.split("_")
 
     #check for the specific phenotype based on the naming convention
     # AND split code into GIFT and GWAS specific code
+
     if csv_files[2] == "Mo98" and csv_files[3]=='whole':# GIFT code vvvvv
         
         #Total_GIFT+=1 (in function instead)
         Current_Mo98_dataframe=pandas.read_csv(csv_file)
 
-        Mo98_ALL_SNPS_GIFT_df,Total_GIFT=GATHER_ALL_GIFT_SNPS(Current_Mo98_dataframe,Mo98_ALL_SNPS_GIFT_df,Total_GIFT,int(csv_files[6]))
+        Mo98_ALL_SNPS_GIFT_df,Total_GIFT_Mo98=GATHER_ALL_GIFT_SNPS(Current_Mo98_dataframe,Mo98_ALL_SNPS_GIFT_df,Total_GIFT_Mo98,int(csv_files[6]))
 
         ''' THRESHOLD CODE WIP
-
         # calculate thresholds (1) and (2) for each p value (pSNP4 and pSNP5 (and abs theta?))
-
         # threshold (1.1) pSNP4 BYH threshold
         m = len(Current_Mo98_dataframe)
         Current_Mo98_dataframe.sort_values(by=["pSNP4"], axis=0, ascending=True,inplace=True, na_position='first')
@@ -346,9 +463,9 @@ for csv_file in csv_files:
         positive_control_chromosome = 2
         positive_control_lower_bound_position = 10933005
         positive_control_upper_bound_position = 10934604
-        '''
+
         for current_index, current_row in Current_Mo98_dataframe.iterrows():
-            ''' THRESHOLD CODE WIP
+             THRESHOLD CODE WIP
             if positive_control_lower_bound_position<=int(current_row["POS"])<=positive_control_upper_bound_position:
                 # then check if its significant or not for each type of pval
                 if float(current_row["pSNP4"])>= BHY_pSNP4_thres:
@@ -361,49 +478,14 @@ for csv_file in csv_files:
                 if float(current_row["pSNP4"])>= BF_THRES_pSNP4:
                     #print("This SNP is significant for pSNP4 under BF")
                     pass
-            '''
-            #AVERAGE MANHATTAN PLOT CODE
-            in_all_SNPS=False
-            for ALL_SNPS_index, ALL_SNPS_row in Mo98_ALL_SNPS_GIFT_df.iterrows():
-                    
-                #check if the current SNP for the current subsample matches anything in the ALL_SNPS dataframe
-                if int(current_row['POS']) == int(ALL_SNPS_row['POS']) and int(current_row['CHR']) == int(ALL_SNPS_row['CHR']) and int(csv_files[6]) == int(ALL_SNPS_row['SUBSAMPLE_NUM']):
-                    
-                    # set flag to true
-                    in_all_SNPS=True
-
-                    # update the row that its on (no need to update CHR, POS or SUBSAMPLE_NUM -> just the rest)
-                    Mo98_ALL_SNPS_GIFT_df.at[ALL_SNPS_index,'TOTAL_PSNP4']= float(ALL_SNPS_row['TOTAL_PSNP4']) + float(current_row['pSNP4'])
-                    Mo98_ALL_SNPS_GIFT_df.at[ALL_SNPS_index,'TOTAL_PSNP5']= float(ALL_SNPS_row['TOTAL_PSNP5']) + float(current_row['pSNP5'])
-                    Mo98_ALL_SNPS_GIFT_df.at[ALL_SNPS_index,'TOTAL_ABS_THETA']= float(ALL_SNPS_row['TOTAL_ABS_THETA']) + float(current_row['absolute_theta'])
-                    Mo98_ALL_SNPS_GIFT_df.at[ALL_SNPS_index,'TIMES_APPEARED']= int(ALL_SNPS_row['TIMES_APPEARED']) + 1
-                    Mo98_ALL_SNPS_GIFT_df.at[ALL_SNPS_index,'TOTAL_GIFT']= int(ALL_SNPS_row['TOTAL_GIFT']) + 1
-
-            # if it doesnt find this SNP in the main dataframe, then add it to the main dataframe for all SNPs
-            if in_all_SNPS==False:
-                 # add new data to dataframe
-                 # add in a line for the current line of data in the calculation 
-                 # NOT SURE IF NEED TO CONVERT TO INTEGER OR NOT HERE
-                new_row=pandas.Series({
-                                        "CHR":current_row['CHR'],
-                                        "POS":current_row['POS'],
-                                        "SUBSAMPLE_NUM":int(csv_files[6]),
-                                        "TOTAL_PSNP4":current_row['pSNP4'],
-                                        "TOTAL_PSNP5":current_row['pSNP5'],
-                                        "TOTAL_ABS_THETA":current_row['absolute_theta'],
-                                        "TIMES_APPEARED":"1",
-                                        "TOTAL_GIFT":Total_GIFT
-                                       })
-                Mo98_ALL_SNPS_GIFT_df =pandas.concat([Mo98_ALL_SNPS_GIFT_df, new_row.to_frame().T], ignore_index=True)
-
-                # re-sort the df so CHR and POS go in ascending order -> it looks nicer that way.
-                Mo98_ALL_SNPS_GIFT_df.sort_values(by=["CHR","POS"], axis=0, ascending=[True,True],inplace=True, na_position='first')
-
+        '''
+            
     elif csv_files[2]=="Mo98" and csv_files[3]=='GWAS':# GWAS code vvv      
         #Total_GWAS+=1
         Current_Mo98_dataframe=pandas.read_csv(csv_file)
-        Mo98_ALL_SNPS_GWAS_df,Total_GWAS = GATHER_ALL_GWAS_SNPS(Current_Mo98_dataframe,Mo98_ALL_SNPS_GWAS_df,Total_GWAS, int(csv_files[6]))
-
+        Mo98_ALL_SNPS_GWAS_df,Total_GWAS_Mo98 = GATHER_ALL_GWAS_SNPS(Current_Mo98_dataframe,Mo98_ALL_SNPS_GWAS_df,Total_GWAS_Mo98, int(csv_files[6]))
+        
+        ''' THRESHOLD CODE WIP
         # calculate thresholds (1) and (2) for GWAS p values
 
         # threshold (1.1) pSNP4 BYH threshold
@@ -435,13 +517,13 @@ for csv_file in csv_files:
         # now cycle through the csv and find positive control SNPs in the MOT1 gene
         # MOT1 gene location boundaries 
 
-        ''' THRESHOLD CODE WIP
+        THRESHOLD CODE WIP
         positive_control_chromosome = 2
         positive_control_lower_bound_position = 10933005
         positive_control_upper_bound_position = 10934604
-        '''
+
         for current_index, current_row in Current_Mo98_dataframe.iterrows():
-            '''
+
             if positive_control_lower_bound_position<=int(current_row["POS"])<=positive_control_upper_bound_position:
                 # then check if its significant or not for each type of pval
                 if float(current_row["pSNP4"])>= BHY_pSNP4_thres:
@@ -453,44 +535,60 @@ for csv_file in csv_files:
                                 already_there = True
                 if float(current_row["pSNP4"])>= BF_THRES_pSNP4:
                     print("This SNP is significant for pSNP4 under BF")
-            '''
-            #AVERAGE MANHATTAN PLOT CODE
-            in_all_SNPS=False
-            for ALL_SNPS_index, ALL_SNPS_row in Mo98_ALL_SNPS_GWAS_df.iterrows():
-                    
-                #check if the current SNP for the current subsample matches anything in the ALL_SNPS dataframe
-                if int(current_row['positions']) == int(ALL_SNPS_row['POS']) and int(current_row['chromosomes']) == int(ALL_SNPS_row['CHR']) and int(csv_files[6]) == int(ALL_SNPS_row['SUBSAMPLE_NUM']):
-                    
-                    # set flag to true
-                    in_all_SNPS=True
 
-                    # update the row that its on (no need to update CHR, POS or SUBSAMPLE_NUM -> just the rest)
-                    Mo98_ALL_SNPS_GWAS_df.at[ALL_SNPS_index,'TOTAL_P']= float(ALL_SNPS_row['TOTAL_P']) + float(current_row['pvals'])
-                    Mo98_ALL_SNPS_GWAS_df.at[ALL_SNPS_index,'TIMES_APPEARED']= int(ALL_SNPS_row['TIMES_APPEARED']) + 1
-                    Mo98_ALL_SNPS_GWAS_df.at[ALL_SNPS_index,'TOTAL_GWAS']= int(ALL_SNPS_row['TOTAL_GWAS']) + 1
+        '''
 
-            # if it doesnt find this SNP in the main dataframe, then add it to the main dataframe for all SNPs
-            if in_all_SNPS==False:
-                 # add new data to dataframe
-                 # add in a line for the current line of data in the calculation 
-                 # NOT SURE IF NEED TO CONVERT TO INTEGER OR NOT HERE
-                new_row=pandas.Series({
-                                        "CHR":current_row['chromosomes'],
-                                        "POS":current_row['positions'],
-                                        "SUBSAMPLE_NUM":int(csv_files[6]),
-                                        "TOTAL_P":current_row['pvals'],
-                                        "TIMES_APPEARED":"1", # not sure if string or int here
-                                        "TOTAL_GWAS":Total_GWAS
-                                       })
-                Mo98_ALL_SNPS_GWAS_df =pandas.concat([Mo98_ALL_SNPS_GWAS_df, new_row.to_frame().T], ignore_index=True)
+    elif csv_files[2] == "Na23" and csv_files[3]=='whole':
+        Current_Na23_dataframe=pandas.read_csv(csv_file) 
+        Na23_ALL_SNPS_GIFT_df,Total_GIFT_Na23=GATHER_ALL_GIFT_SNPS(Current_Na23_dataframe,Na23_ALL_SNPS_GIFT_df,Total_GIFT_Na23,int(csv_files[6]))
 
-                # re-sort the df so CHR and POS go in ascending order -> it looks nicer that way.
-                Mo98_ALL_SNPS_GWAS_df.sort_values(by=["CHR","POS"], axis=0, ascending=[True,True],inplace=True, na_position='first')
+    elif csv_files[2] == "Na23" and csv_files[3]=='GWAS':
+        #Total_GWAS+=1
+        Current_Na23_dataframe=pandas.read_csv(csv_file)
+        Na23_ALL_SNPS_GWAS_df,Total_GWAS_Na23 = GATHER_ALL_GWAS_SNPS(Current_Na23_dataframe,Na23_ALL_SNPS_GWAS_df,Total_GWAS_Na23, int(csv_files[6]))
+
+    else:
+        print("failure to detect phenotype and GWAS/GIFT method")
+
+# Now go through each of the total SNP dataframes and calculate the average P values for each SNP
+Mo98_ALL_SNPS_GIFT_df = CALCULATE_AVERAGE_SNPS_GIFT(Mo98_ALL_SNPS_GIFT_df)
+Mo98_ALL_SNPS_GWAS_df = CALCULATE_AVERAGE_SNPS_GWAS(Mo98_ALL_SNPS_GWAS_df)
+
+Na23_ALL_SNPS_GIFT_df = CALCULATE_AVERAGE_SNPS_GIFT(Na23_ALL_SNPS_GIFT_df)
+Na23_ALL_SNPS_GWAS_df = CALCULATE_AVERAGE_SNPS_GWAS(Na23_ALL_SNPS_GWAS_df)
+
+# write results to csv files for an R script to run them
+Mo98_ALL_SNPS_GIFT_df.to_csv("output_files/Mo98_ALL_SNPS_GIFT.csv", index=False)
+Mo98_ALL_SNPS_GWAS_df.to_csv("output_files/Mo98_ALL_SNPS_GWAS.csv", index=False)
+
+Na23_ALL_SNPS_GIFT_df.to_csv("output_files/Na23_ALL_SNPS_GIFT.csv", index=False)
+Na23_ALL_SNPS_GWAS_df.to_csv("output_files/Na23_ALL_SNPS_GWAS.csv", index=False)
+
+subsample_num_list=[200,400,600,800,1000]
+phenotype_list=["Mo98","Na23"]
+pvals=["TOTAL_P","TOTAL_PSNP4","TOTAL_PSNP5","TOTAL_ABS_THETA"]
+# R script creation and running
+for phenotype in phenotype_list:
+
+    pass
+
+    for subsample_number in subsample_num_list:
+
+        for pval_type in pvals:
+
+            MAKE_R_SCRIPT(phenotype,subsample_number,pval_type)
+          
+            
+            
+            pass
 
 
-    if csv_files[2] == "Na23":
-        print("Na23")
-        # do same code as for Mo_98 but pass different dataframe into the function (Na23 instead of Mo98 df)
+
+
+
+
+
+
 
 
 
